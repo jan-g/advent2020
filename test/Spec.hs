87a2e43779b5b2d10b35bac16e90c79ffef73bdf
jang <jang@ioctl.org>
2020-12-01 10:27:58 +0000

    Day1 part a

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,20 @@
+import Test.Hspec
+import Control.Exception (evaluate)
+
+import Data.Array
+import Data.Function ((&))
+import qualified Data.Map.Strict as Map
+import qualified Data.Set as Set
+import Control.Monad
+import Data.List.Split (splitOn)
+
+import qualified Day1
+
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main =
+  hspec $ do
+    describe "Day1" $ do
+      it "correctly runs on the test data" $ do
+        (lines Day1.testData & Day1.parse) `shouldBe` [1721, 979, 366, 299, 675, 1456]
+        Day1.day1 (lines Day1.testData) `shouldBe` 1721 * 299
